4b141454afea4a468485eb36493d68acc5bf8e20
Praveen Kumar <praveenscience@gmail.com>
2024-08-11 10:07:42 +0100

    Added morgan for logging requests.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // Import Express JS to use in our app.
 const express = require("express");
+// Importing Morgan for who knows what?
+const morgan = require("morgan");
 // We need to create an instance of this Express JS.
 const server = express();
 // Define a port for our backend.
@@ -9,6 +11,9 @@ const port = 5000;
 server.get("/", (req, res) => {
   res.json("Hello Codess Cafe!");
 });
+
+// Sample ToDo App
+server.use(morgan("dev"));
 
 // Make the server listen to this port.
 server.listen(port, () => {
