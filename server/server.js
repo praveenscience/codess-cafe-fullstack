826cd1852e464c9e9f15854dbcce2e99e833f494
Praveen Kumar <praveenscience@gmail.com>
2024-08-11 10:42:25 +0100

    Sessions: Helps the server remember who you are.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 // Import Express JS to use in our app.
 const express = require("express");
-// Importing Morgan for who knows what?
+// Importing Morgan for Dev Logs.
 const morgan = require("morgan");
+// Import Sessions Support.
+const session = require("express-session");
 // We need to create an instance of this Express JS.
 const server = express();
 // Define a port for our backend.
@@ -14,8 +16,21 @@ server.get("/", (req, res) => {
   res.json("Hello Codess Cafe!");
 });
 
-// Sample ToDo App
+// Show the dev logs.
 server.use(morgan("dev"));
+// Session Config
+const sessionConfig = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+};
+if (server.get("env") === "production") {
+  server.set("trust proxy", 1); // trust first proxy
+  sessionConfig.cookie.secure = true; // serve secure cookies
+}
+// Enable sessions!
+server.use(session(sessionConfig));
 // Read the POST Data
 server.use(express.json());
 // Bring in the ToDos.
