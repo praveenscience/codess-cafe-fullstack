55e57594a84a7858d08f3d2667dd102832f543b9
Praveen Kumar <praveenscience@gmail.com>
2024-08-11 10:20:12 +0100

    Almost complete ToDo App with Body Parser.

--- server/server.js
+++ server/server.js
@@ -14,6 +14,55 @@ server.get("/", (req, res) => {
 
 // Sample ToDo App
 server.use(morgan("dev"));
+// Read the POST Data
+server.use(express.json());
+// Simple Sample Database
+const ToDos = ["Running", "Coding", "Reading", "Cooking", "Journalling"];
+// Get Everything.
+server.get("/todos", (req, res) => {
+  res.json(ToDos);
+});
+// Get Something Specific.
+server.get("/todos/:id", (req, res) => {
+  if (typeof ToDos[req.params.id] === "undefined") {
+    res.status(404).json("Not Found");
+  } else {
+    res.json(ToDos[req.params.id]);
+  }
+});
+// Create a new Record
+server.post("/todos", (req, res) => {
+  if (typeof req.body.Item === "undefined") {
+    res.status(406).json("Please send Item.");
+  } else {
+    ToDos.push(req.body.Item);
+    res.status(201).json(ToDos.length - 1);
+  }
+});
+// Editing a Record.
+server.put("/todos/:id", (req, res) => {
+  if (typeof ToDos[req.params.id] === "undefined") {
+    res.status(404).json("Not Found");
+  } else {
+    if (typeof req.body.Item === "undefined") {
+      res.status(400).json("You have to specify new value using Item.");
+    } else {
+      ToDos[req.params.id] = req.body.Item;
+      console.log(`Updated Record #${req.params.id} to ${req.body.Item}.`);
+      res.status(202).json(ToDos[req.params.id]);
+    }
+  }
+});
+// Deleting a Record.
+server.delete("/todos/:id", (req, res) => {
+  if (typeof ToDos[req.params.id] === "undefined") {
+    res.status(404).json("Not Found");
+  } else {
+    ToDos[req.params.id] = undefined;
+    console.log(`Deleted Record #${req.params.id}.`);
+    res.status(204).end();
+  }
+});
 
 // Make the server listen to this port.
 server.listen(port, () => {
